9a3fbf5b2be54ea58235b105484e391954a9a865
SheikhIshere <thesheikh255@gmail.com>
2025-09-24 01:37:30 +0600

    did test.py and compleated chapter 5 from django for professonal book

--- 3bookstore/bookstore/settings.py
+++ 3bookstore/bookstore/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "accounts.apps.AccountsConfig",
+    'accounts.apps.AccountsConfig',
+    'pages.apps.PagesConfig',
+
 ]
 AUTH_USER_MODEL = "accounts.CustomUser"
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
