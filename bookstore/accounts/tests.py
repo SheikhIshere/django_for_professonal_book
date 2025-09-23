a6c19e9114d440cd7e048b1f31201cca509c9350
SheikhIshere <thesheikh255@gmail.com>
2025-09-23 23:50:46 +0600

    bookstore from django for professonal

---
+++ bookstore/accounts/tests.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+class CustomUserTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='miaK', email='miak@hotmail.com', password='testpass123'            
+        )
+
+        self.assertEqual(user.username, 'miaK')
+        self.assertEqual(user.email, 'miak@hotmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="superadmin", email="superadmin@email.com", password="testpass123"
+        )
+        
+        self.assertEqual(admin_user.username, "superadmin")
+        self.assertEqual(admin_user.email, "superadmin@email.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
