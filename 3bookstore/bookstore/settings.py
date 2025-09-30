736c9164f6c139263fb61dee06215fb2387207d7
SheikhIshere <thesheikh255@gmail.com>
2025-10-01 00:46:08 +0600

    ch 13 done

--- 3bookstore/bookstore/settings.py
+++ 3bookstore/bookstore/settings.py
@@ -157,6 +157,10 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
+# for media
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- 3bookstore/bookstore/urls.py
+++ 3bookstore/bookstore/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 # bookstore/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,5 +32,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path("books/", include("books.urls")),  # new
 
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
